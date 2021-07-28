e5fe45b59a0a2b6d90b88b1dae5eded1a97b00ba
vaa2 <vaa2@aluno.ifal.edu.br>
2021-07-28 01:44:29 +0000

    Adicionado testes

--- stack.js
+++ stack.js
@@ -1,29 +1,24 @@
-    function stack() {       
+        function stack() {       
 
         this.items = [];  
         
-        //Qual o tamanho da pilha
         this.size = function() {     
         return this.items.length;   
         }
     
-        //Verificar se a pilha ta vazia ou não
-    this.isEmpty = function() {     
+        this.isEmpty = function() {     
         return this.items.length === 0;   
     }
     
-        //Colocar elementos na pilha
-    this.push = function(element) {
+        this.push = function(element) {
         this.items.push(element);   
     }
-    
-        //Remove elementos da pilha
-    this.pop = function() {   
+
+        this.pop = function() {   
         if (this.isEmpty())
             throw "pilha vazia"  
         return this.items.pop();   
     }
     }
-    
-    //Exportar elemento stack
+ 
     module.exports = stack;
